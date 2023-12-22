8e51550b50d304a95c47bdba179bc35041c07225
manevolent <jproctor796@gmail.com>
2023-12-22 16:05:01 -0700

    Little bit of cleanup, add some extra keys (levels 3 and 5)

--- java/src/main/java/com/github/manevolent/atlas/Main.java
+++ java/src/main/java/com/github/manevolent/atlas/Main.java
@@ -5,6 +5,7 @@ import com.github.manevolent.atlas.j2534.J2534DeviceDescriptor;
 import com.github.manevolent.atlas.j2534.serial.SerialTatrixOpenPortFactory;
 import com.github.manevolent.atlas.j2534.tactrix.SerialTactrixOpenPort;
 import com.github.manevolent.atlas.ssm4.Crypto;
+import com.github.manevolent.atlas.subaru.SubaruDITCommands;
 import com.github.manevolent.atlas.subaru.SubaruProtocols;
 import com.github.manevolent.atlas.subaru.SubaruSecurityAccessCommandAES;
 import com.github.manevolent.atlas.subaru.uds.request.SubaruStatus1Request;
@@ -68,8 +69,7 @@ public class Main {
                 Thread.sleep(250);
 
                 System.out.println("Unlocking CGW...");
-                new SubaruSecurityAccessCommandAES(0x7, CENTRAL_GATEWAY, Crypto.toByteArray("7692E7932F23A901568DDFA5FF580625"))
-                        .execute(session);
+                SubaruDITCommands.SECURITY_ACCESS_LEVEL_7.execute(session);
 
                 System.out.println("Starting routine...");
                 session.request(
@@ -100,47 +100,21 @@ public class Main {
                         new UDSRoutineControlRequest(RoutineControlSubFunction.START_ROUTINE, 0x2, new byte[]{0x1})
                 );
 
-
-
-                session.writer().write(ENGINE_1, new UDSSecurityAccessRequest(61, new byte[0]));
-
-                System.out.println("Unlocking ECU...");
-                new SubaruSecurityAccessCommandAES(0x1, ENGINE_1, Crypto.toByteArray("667E3078219976B4EDF3D43BD1D8FFC9"))
-                        .execute(session);
-
-                /**System.out.println("Entering programming session...");
-                session.request(
-                        ENGINE_1,
-                        new UDSDiagSessionControlRequest(DiagnosticSessionType.PROGRAMMING_SESSION)
-                );
-
-                Thread.sleep(250);
-                System.out.println("Starting routine...");
-                session.writer().write(BROADCAST, new UDSTesterPresentRequest((byte) 0x80));
-
-                 // CLEARS FLASH!
-                //new SubaruClearFlashCommand(ENGINE_1, 0x000100000, 0x003F0000).execute(session);
-                **/
-
+                SubaruDITCommands.SECURITY_ACCESS_LEVEL_1.execute(session);
                 break;
-
             } catch (Exception ex) {
                 ex.printStackTrace();
                 Thread.sleep(1000L);
             }
         }
 
+        session.request(
+                ENGINE_1,
+                new UDSDiagSessionControlRequest(DiagnosticSessionType.EXTENDED_SESSION)
+        );
+
         System.out.println("Reading memory...");
-        for (int offs = 0; offs < 1024; offs ++) {
-            session.writer().write(BROADCAST, new UDSTesterPresentRequest((byte) 0x80));
-
-            System.out.println("Reading memory by address...");
-            session.request(
-                    ENGINE_1,
-                    new UDSReadMemoryByAddressRequest(1, 255, 4, 255)
-            );
-
-        }
+        session.request(ENGINE_1, new UDSReadMemoryByAddressRequest(4, 0x0FFF, 1, 16));
     }
 
 }
--- java/src/main/java/com/github/manevolent/atlas/subaru/SubaruDITCommands.java
+++ java/src/main/java/com/github/manevolent/atlas/subaru/SubaruDITCommands.java
@@ -11,8 +11,7 @@ import com.github.manevolent.atlas.uds.command.UDSSupplier;
 
 import java.util.Set;
 
-import static com.github.manevolent.atlas.subaru.SubaruDITComponent.ENGINE_1;
-import static com.github.manevolent.atlas.subaru.SubaruDITComponent.ENGINE_2;
+import static com.github.manevolent.atlas.subaru.SubaruDITComponent.*;
 
 public final class SubaruDITCommands {
 
@@ -24,11 +23,30 @@ public final class SubaruDITCommands {
                 }
             };
 
-    public static final UDSSecurityAccessCommand SECURITY_ACCESS_7_8 =
-            new SubaruSecurityAccessCommandAES(0x7, ENGINE_2, Crypto.toByteArray("7692E7932F23A901568DDFA5FF580625"));
+    /**
+     * Used on the CGW (Central Gateway) module
+     */
+    public static final UDSSecurityAccessCommand SECURITY_ACCESS_LEVEL_7 =
+            new SubaruSecurityAccessCommandAES(0x7, CENTRAL_GATEWAY, Crypto.toByteArray("7692E7932F23A901568DDFA5FF580625"));
 
-    public static final UDSSecurityAccessCommand SECURITY_ACCESS_1_2 =
-            new SubaruSecurityAccessCommandAES(0x1, ENGINE_2, Crypto.toByteArray("667E3078219976B4EDF3D43BD1D8FFC9"));
+    /**
+     * Allows for a programming session and for flash to be written to
+     */
+    public static final UDSSecurityAccessCommand SECURITY_ACCESS_LEVEL_1 =
+            new SubaruSecurityAccessCommandAES(0x1, ENGINE_1, Crypto.toByteArray("667E3078219976B4EDF3D43BD1D8FFC9"));
+
+    /**
+     * Unknown purpose
+     */
+    public static final UDSSecurityAccessCommand SECURITY_ACCESS_LEVEL_3 =
+            new SubaruSecurityAccessCommandAES(0x3, ENGINE_1, Crypto.toByteArray("469A20AB308D5CA64BCD5BBE535BD85F"));
+
+    /**
+     * Unknown purpose
+     */
+    public static final UDSSecurityAccessCommand SECURITY_ACCESS_LEVEL_5 =
+            new SubaruSecurityAccessCommandAES(0x5, ENGINE_1, Crypto.toByteArray("E8CC52D5D8F20706424813126FA7ABDD"));
+
 
     public static final UDSSupplier<SubaruReadDTCRequest, SubaruReadDTCResponse, Set<Short>>
             READ_DTC = new UDSSupplier<>() {
